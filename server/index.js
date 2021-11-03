9d2bf74b186230c23dcb544211ad6e3447182a64
andrewphm <andrpham@gmail.com>
2021-11-03 14:43:48 -0400

    Set up node server using express. Create middleware, routes, and controllers for authentication

--- server/index.js
+++ server/index.js
@@ -3,7 +3,31 @@ const express = require('express');
 // cross-origin requests
 const cors = require('cors');
 
+const authRoutes = require('./routes/auth.js');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Allow us to call env variables inside our node application
 require('dotenv').config();
+
+// Setup middleware
+
+// All us to make cross-orgin requests
+app.use(cors());
+
+// parse json
+app.use(express.json());
+// parse form data
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('hello, world!');
+});
+
+app.use('/auth', authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}...`);
+});
