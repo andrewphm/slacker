ebf46f9ce0d3f999de21f30c94c5bed178d12447
andrewphm <andrpham@gmail.com>
2021-11-03 10:12:42 -0400

    create Channel container and channel list container component and create style sheet

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React from 'react';
+
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+
+import Cookies from 'universal-cookie';
+
+// components
+import { ChannelListContainer, ChannelContainer } from './components';
+
+// style
+import './App.css';
+
+const apiKey = 'uqb5m9wf2y2s';
+const client = StreamChat.getInstance(apiKey);
 
 const App = () => {
-  return <div>hello world</div>;
+  return (
+    <div className="app_wrapper">
+      <Chat client={client} theme="team light">
+        <ChannelListContainer />
+        <ChannelContainer />
+      </Chat>
+    </div>
+  );
 };
 
 export default App;
---
+++ client/src/components/ChannelListContainer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChannelListContainer = () => {
+  return <div>Channel List Container</div>;
+};
+
+export default ChannelListContainer;
